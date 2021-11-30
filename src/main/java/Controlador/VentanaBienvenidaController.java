1621543311bf379a73cb33ae866a5ffc9da72157
JAvillarreal07 <jvillarrealoliver@safareyes.es>
2021-11-30 11:59:42 +0100

    Atope2

--- src/main/java/Controlador/VentanaBienvenidaController.java
+++ src/main/java/Controlador/VentanaBienvenidaController.java
@@ -1,4 +1,28 @@
 package Controlador;
 
-public class VentanaBienvenidaController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author josea
+ */
+public class VentanaBienvenidaController implements Initializable {
+
+
+    public JFXButton botonAlmacen, botonGestion, botonEmpleados;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        //botonAlmacen.setStyle("-fx-background-color: purple");
+
+    }
+
 }
