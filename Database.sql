2913aed357846b77303ea78f257a83a99880e41f
Jesús <jcrucessoto@safareyes.es>
2021-11-30 12:23:25 +0100

    He sido yo

---
+++ Database.sql
@@ -0,0 +1,84 @@
+CREATE DATABASE kamonduck;
+
+CREATE TABLE Patos
+(
+    IDPato INT AUTO_INCREMENT,
+    Nombre_Pato VARCHAR(50) NOT NULL,
+    Raza VARCHAR(10) NOT NULL,
+    Edad INT NOT NULL,
+    Num_Cartilla INT NOT NULL,
+    Descripcion VARCHAR (100),
+    PRIMARY KEY (IDPato)
+);
+
+CREATE TABLE Clientes
+(
+    IDCliente INT AUTO_INCREMENT,
+    DNI CHAR(9) NOT NULL,
+    Nombre VARCHAR(50) NOT NULL,
+    Apellidos VARCHAR(50) NOT NULL,
+    Telefono1 CHAR(9) NOT NULL,
+    Telefono2 CHAR(9),
+    Email VARCHAR(50) NOT NULL,
+    TipoPago VARCHAR(20) NOT NULL,
+    PRIMARY KEY (IDCliente)
+);
+
+CREATE TABLE Lagos
+(
+    IDLago INT NOT NULL,
+    TarifaPato INT NOT NULL,
+    Tamanho VARCHAR(50) NOT NULL,
+    Cap_Patos INT NOT NULL,
+    Nombre_Lago VARCHAR(50) NOT NULL,
+    PRIMARY KEY (IDLago)
+);
+
+CREATE TABLE Empleados
+(
+    IDEmpleado INT AUTO_INCREMENT,
+    Nombre_Empleado VARCHAR(50) NOT NULL,
+    Apellidos_Empleado VARCHAR(50) NOT NULL,
+    Cargo VARCHAR(10) NOT NULL,
+    Horario_Trabajo VARCHAR(10) NOT NULL,
+    Turno VARCHAR(20) NOT NULL,
+    Telefono_Empleado CHAR(9) NOT NULL,
+    IDLago INT NOT NULL,
+    PRIMARY KEY (IDEmp),
+    FOREIGN KEY (IDLago) REFERENCES Lagos(IDLago)
+);
+
+CREATE TABLE Proveedor
+(
+    IDProveedor INT AUTO_INCREMENT,
+    Nombre VARCHAR(100) NOT NULL,
+    Dirección VARCHAR(100) NOT NULL,
+    Telefono_Proveedor CHAR NOT NULL,
+    Pais VARCHAR(50) NOT NULL,
+    PRIMARY KEY (IDProveedor)
+);
+
+CREATE TABLE Estancia
+(
+    IDEstancia INT AUTO_INCREMENT,
+    Fecha_Ingreso DATE NOT NULL,
+    Fecha_Salida DATE NOT NULL,
+    IDPato INT NOT NULL,
+    IDCliente INT NOT NULL,
+    IDLago INT NOT NULL,
+    PRIMARY KEY (IDEstancia),
+    FOREIGN KEY (IDPato) REFERENCES Patos(IDPato),
+    FOREIGN KEY (IDCliente) REFERENCES Clientes(IDCliente),
+    FOREIGN KEY (IDLago) REFERENCES Lagos(IDLago)
+);
+
+CREATE TABLE Productos
+(
+    IDProducto INT NOT NULL,
+    Nombre_Producto VARCHAR(100) NOT NULL,
+    Cantidad INT NOT NULL,
+    Observaciones VARCHAR(250),
+    IDProveedor INT NOT NULL,
+    PRIMARY KEY (IDProducto),
+    FOREIGN KEY (IDProveedor) REFERENCES Proveedor(IDProveedor)
+);
--- src/main/java/Modelo/Estancia.java
+++ src/main/java/Modelo/Estancia.java
@@ -1,4 +1,41 @@
 package Modelo;
 
+import java.util.Date;
+
 public class Estancia {
+    private int IDEstancia, IDCliente, IDPato, IDLago;
+    private Date Fecha_Ingreso, Fecha_Salida;
+
+    public Estancia(int IDEstancia, int IDCliente, int IDPato, int IDLago, Date fecha_Ingreso, Date fecha_Salida) {
+        this.IDEstancia = IDEstancia;
+        this.IDCliente = IDCliente;
+        this.IDPato = IDPato;
+        this.IDLago = IDLago;
+        Fecha_Ingreso = fecha_Ingreso;
+        Fecha_Salida = fecha_Salida;
+    }
+
+    public int getIDEstancia() {return IDEstancia;}
+
+    public void setIDEstancia(int IDEstancia) {this.IDEstancia = IDEstancia;}
+
+    public int getIDCliente() {return IDCliente;}
+
+    public void setIDCliente(int IDCliente) {this.IDCliente = IDCliente;}
+
+    public int getIDPato() {return IDPato;}
+
+    public void setIDPato(int IDPato) {this.IDPato = IDPato;}
+
+    public int getIDLago() {return IDLago;}
+
+    public void setIDLago(int IDLago) {this.IDLago = IDLago;}
+
+    public Date getFecha_Ingreso() {return Fecha_Ingreso;}
+
+    public void setFecha_Ingreso(Date fecha_Ingreso) {Fecha_Ingreso = fecha_Ingreso;}
+
+    public Date getFecha_Salida() {return Fecha_Salida;}
+
+    public void setFecha_Salida(Date fecha_Salida) {Fecha_Salida = fecha_Salida;}
 }
