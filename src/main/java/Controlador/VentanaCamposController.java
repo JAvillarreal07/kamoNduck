3a9ca781b3cc0ca4dcb15bff170a7f5b7cfb6e83
sara_repeto <srepetogarcia@safareyes.es>
2021-12-14 12:35:16 +0100

    valepato

--- src/main/java/Controlador/VentanaCamposController.java
+++ src/main/java/Controlador/VentanaCamposController.java
@@ -6,7 +6,6 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
-import javafx.stage.Stage;
 
 import java.io.File;
 
@@ -14,9 +13,9 @@ public class VentanaCamposController {
     @FXML
         private AnchorPane anchorpane;
 
-        private String pathdelimage;
+        public String pathdelimage;
         public ImageView imagen;
-    public void HandleButtonAction(ActionEvent actionEvent) {
+    public void HandleButtonAction(ActionEvent event) {
 
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Abrir archivo");
@@ -25,11 +24,13 @@ public class VentanaCamposController {
         FileChooser.ExtensionFilter extFilterPNG = new
                 FileChooser.ExtensionFilter("Archivos PNG", "*.PNG");
         fileChooser.getExtensionFilters().addAll(extFilterJPG, extFilterPNG);
-        if (fileChooser!=null){
+
             File file = fileChooser.showOpenDialog(null);
             imagen.setImage(new Image(file.toURI().toString()));
-            String urlDeLaImagen = file.toURI().toString();
-        }
+
+            //por si hace falta tener el path aqui va
+            pathdelimage = file.toURI().toString();
+
 
 
     }
