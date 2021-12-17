f1f86782befc7fa4f2c0c70ca2663cc3236cd72c
sara_repeto <srepetogarcia@safareyes.es>
2021-12-17 13:46:35 +0100

    valepato

--- src/main/java/Controlador/VentanaCamposController.java
+++ src/main/java/Controlador/VentanaCamposController.java
@@ -1,5 +1,6 @@
 package Controlador;
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
@@ -7,7 +8,10 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
 public class VentanaCamposController {
     @FXML
@@ -15,6 +19,8 @@ public class VentanaCamposController {
 
         public String pathdelimage;
         public ImageView imagen;
+        public Image image;
+        public String nombrenuevo;
     public void HandleButtonAction(ActionEvent event) {
 
         FileChooser fileChooser = new FileChooser();
@@ -27,11 +33,19 @@ public class VentanaCamposController {
 
             File file = fileChooser.showOpenDialog(null);
             imagen.setImage(new Image(file.toURI().toString()));
-
             //por si hace falta tener el path aqui va
             pathdelimage = file.toURI().toString();
 
-
-
+            image = new Image(pathdelimage);
+    }
+    public void saveToFile() {
+        //nombrenuevo = "francisquita";
+        File outputFile = new File("C:\\Users\\User-01\\Documents\\MTC\\interfaces\\kamoNduck\\src\\main\\resources\\FotosEmpleados\\"+nombrenuevo+".png");
+        BufferedImage bImage = SwingFXUtils.fromFXImage(image, null);
+        try {
+            ImageIO.write(bImage, "png", outputFile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
