68962923db7051023fa183aabf6e571ff4f20799
Jesus <jcrucessoto@safareyes.es>
2022-02-02 18:32:07 +0100

    He sido yo

--- Database.sql
+++ Database.sql
@@ -101,7 +101,7 @@ INSERT INTO empleados VALUES (NULL, 'DNIEjemp', 'Jose Antonio', 'Ejemplo', '1245
 INSERT INTO empleados VALUES (NULL, 'DNIEjemp', 'Freya', 'Ejemplo', '78123659', 'FrViceAdmin@KamoNDuck.com', 'ViceAdmin', 'M-J-S', 'Diurno', 1);
 INSERT INTO empleados VALUES (NULL, '77836512L', 'Benito', 'Ejemplo', '142032012', 'BenBecario@KamoNDuck.com', 'Becario', 'L-M-X-J-V-S-D', 'Nocturno', 2);
 INSERT INTO empleados VALUES (NULL, '78912451W', 'Álvaro', 'Ortiz Castillo', '695647125', 'ÁlvVet@KamoNDuck.com', 'Veterinario', 'L-M-X-J', 'Partido', 1);
-INSERT INTO empleados VALUES (NULL, '69514723G', 'Ángela', 'Ortiz Castillo', '695647125', 'ÁngVet@KamoNDuck.com', 'Veterinario', 'V-S-D', 'Partido', 1);
+INSERT INTO empleados VALUES (NULL, '69514723G', 'Ángela', 'Ortiz Castillo', '695647126', 'ÁngVet@KamoNDuck.com', 'Veterinario', 'V-S-D', 'Partido', 1);
 INSERT INTO empleados VALUES (NULL, '59845125P', 'Miguel', 'Castaño Laredo', '548126589', 'MigMante@KamoNDuck.com', 'Mantenimiento', 'L-X-V-D', 'Diurno', 1);
 INSERT INTO empleados VALUES (NULL, '59845125P', 'Carlos', 'Gómez Pérez', '954214785', 'CarMante@KamoNDuck.com', 'Mantenimiento', 'M-J-S', 'Diurno', 1);
 
@@ -109,18 +109,18 @@ INSERT INTO patos VALUES (NULL, 'Gilito', 'Pekín americano', 11, 220001, 'Está
 INSERT INTO patos VALUES (NULL, 'Jaimito', 'Pekín americano', 2, 220002, 'ALERGIA a las nueces');
 INSERT INTO patos VALUES (NULL, 'Jorgito', 'Pekín americano', 2, 220003, 'Muy sano');
 INSERT INTO patos VALUES (NULL, 'Juanito', 'Pekín americano', 2, 220004, 'ALERGIA al salmón');
-INSERT INTO patos VALUES (NULL, 'Pyduck', 'Pekín blanco', 1, 220006, 'Es una cría, PROBLEMA tiene dolores de cabeza');
+INSERT INTO patos VALUES (NULL, 'Psyduck', 'Pekín blanco', 1, 220006, 'Es una cría, PROBLEMA tiene dolores de cabeza');
 INSERT INTO patos VALUES (NULL, 'Lucas', 'Ánade sombrío', 5, 220008, 'Muy exigente');
 INSERT INTO patos VALUES (NULL, 'Funky', 'Tadorne coreano', 3, 220009, 'Tratar con cuidado, casi extinto');
 INSERT INTO patos VALUES (NULL, 'Iris', 'Mandarín', 4, 220010, 'PROBLEMA está bajo de peso, necesita alimento extra');
 
 
 INSERT INTO clientes VALUES (NULL, '12345678L', 'Walter', 'Elias Disney', '954623154', '954872102','WDisney@Dinero.com', 'Cheque');
-INSERT INTO Clientes VALUES (NULL, '54823621M', 'Mysty', 'Ciudad Celeste', '955632154', '965215487', 'MistyEntrenadoradel91@PCDEBill.com', 'Efectivo');
+INSERT INTO Clientes VALUES (NULL, '54823621M', 'Misty', 'Ciudad Celeste', '955632154', '965215487', 'MistyEntrenadoradel91@PCDEBill.com', 'Efectivo');
 INSERT INTO Clientes VALUES (NULL, '84752136W', 'Harry', 'Warner', '945125478', '666231547', 'HarryElMayor@HermanosWarner.com', 'Paypal');
-INSERT INTO Clientes VALUES (NULL, '84752136W', 'Albert', 'Warner', '945125479', '666231548', 'AlbertoElNoMayor@HermanosWarner.com', 'Efectivo');
-INSERT INTO Clientes VALUES (NULL, '84752136W', 'Sam', 'Warner', '945125479', '666231548', 'SamuelElNoMenor@HermanosWarner.com', 'Cheque');
-INSERT INTO Clientes VALUES (NULL, '84752136W', 'Jack', 'Warner', '945125480', '666231548', 'JacoboElMenor@HermanosWarner.com', 'Transferencia');
+INSERT INTO Clientes VALUES (NULL, '84752137W', 'Albert', 'Warner', '945125479', '666231548', 'AlbertoElNoMayor@HermanosWarner.com', 'Efectivo');
+INSERT INTO Clientes VALUES (NULL, '84752138W', 'Sam', 'Warner', '945125479', '666231549', 'SamuelElNoMenor@HermanosWarner.com', 'Cheque');
+INSERT INTO Clientes VALUES (NULL, '84752139W', 'Jack', 'Warner', '945125480', '666231550', 'JacoboElMenor@HermanosWarner.com', 'Transferencia');
 
 
 INSERT INTO Proveedor VALUES (1, 'Versele-laga', 'Kapellestraat 70 / 9800 Deinze','+32 093813200','Bélgica');
@@ -135,7 +135,7 @@ INSERT INTO Productos VALUES (NULL, 'Pala para excrementos','Limpieza', 2, 1, 25
 INSERT INTO Productos VALUES (NULL, 'Saco 10 Kg Zanahoria','Alimentacion', 5, 3, 10, '-',3);
 INSERT INTO Productos VALUES (NULL, 'Saco 15 Kg Pepino','Alimentacion', 5, 3, 10, '-',3);
 INSERT INTO Productos VALUES (NULL, 'Saco 10 Kg Manzana','Alimentacion', 5, 3, 10, '-',3);
-INSERT INTO Productos VALUES (NULL, 'Saco 20 Kg semillas','Alimentación', 5, 2, 55, 'Elección de ganos y semillas especiales para patos',4);
+INSERT INTO Productos VALUES (NULL, 'Saco 20 Kg semillas','Alimentación', 5, 2, 55, 'Elección de granos y semillas especiales para patos',4);
 
 INSERT INTO Estancia VALUES (NULL, '2021-12-11', '2021-12-24', 5,2,2);
 INSERT INTO Estancia VALUES (NULL, '2021-12-13', '2021-12-15', 6,5,2);
